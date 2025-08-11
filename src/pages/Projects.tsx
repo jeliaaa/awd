76ee7fec209ca9d1676616456c90a599fb13148e
NikaShabashvili <104011629+NikaShabashvili03@users.noreply.github.com>
2025-08-11 14:01:14 +0400

    years in projets

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,10 +1,20 @@
-
 function Projects() {
+    const years = [2021, 2022, 2023, 2024, 2025];
+
     return (
-        <div>
+        <div className="text-center flex flex-col gap-10">
+            <ul className="flex gap-4 text-primary plain-text">
+                {years.map((year) => (
+                    <li key={year}>
+                        <button className="cursor-pointer hover:underline">
+                            {year}
+                        </button>
+                    </li>
+                ))}
+            </ul>
             Projects
         </div>
-    )
+    );
 }
 
-export default Projects
+export default Projects;
